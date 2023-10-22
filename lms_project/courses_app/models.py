00e44a80ecabcd399e198a4f7d00a384bdb1a6b7
Sergey Malyarov <sergey@malyaroff.com>
2023-10-22 17:00:31 +0300

    create a courses_app

--- lms_project/courses_app/models.py
+++ lms_project/courses_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Course(models.Model):
     title = models.CharField(max_length=255)
     preview_image = models.ImageField(upload_to='course_previews/', null=True, blank=True)
